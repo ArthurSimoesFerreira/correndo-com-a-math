db884c1b73a26c172d9814685c1968ed1d4e5735
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-10-21 13:21:06 -0300

    Mudança nos nomes no menu.py e na altura e largura dos objects

--- menu.py
+++ menu.py
@@ -11,30 +11,26 @@ width_window = window.width
 height_window = window.height
 window.set_title("menu")
 window.set_background_color((0, 0, 0))
-background = Sprite("background_menu_jogofinal.jpg", 1)
+background = Sprite("assets\\background_menu.png", 1)
 background.x=0
 background.y=0
 mouse = Window.get_mouse()
 keyboard = Window.get_keyboard()
 
 # ------ buttons ------ #
-start= Sprite("start.png",1)
-width_start=260
-height_start=125
-start.x = ((width_window)/2 - (width_start)/2)
-start.y = ((height_window)/2 + (height_start * 3/2))
+start= Sprite("assets\\button_start.png",1)
+start.x = ((width_window)/2 - (start.width)/2)
+start.y = ((height_window)/2 + (start.height * 3/1.8))
 
 # ----- story panel ----- #
-width_panel= 570
-height_panel= 365
-panel = Sprite("storypanel.png",1)
-panel.x= ((width_window)/2 - (width_panel)/2)
-panel.y= ((height_window)/2 - (height_panel)*2/3)
+story_window = Sprite("assets\\window_story.png",1)
+story_window.x= ((width_window)/2 - (story_window.width)/2)
+story_window.y= ((height_window)/2 - (story_window.height)*2/3)
 
 while True:
     # ------------------- #
     background.draw()
-    panel.draw()
+    story_window.draw()
     start.draw()
 
     # ----- start ------ #
